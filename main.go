9122cd0823f0698b6ad223879f3689ff71450c8a
coderj001 <amirajubolchi001@gmail.com>
2021-11-23 11:05:23 +0530

    update urlcheck function

--- main.go
+++ main.go
@@ -1,26 +1,41 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/url"
+	"strings"
 )
 
 func main() {
-    var in_url string
+	var in_url string
 
-    fmt.Println(" :Welcome To Amazon Prize Tracker: ")
-    fmt.Print("Enter URL: ")
-    fmt.Scanln(&in_url)
+	fmt.Println(" :Welcome To Amazon Prize Tracker: ")
+	fmt.Print("Enter URL: ")
+	fmt.Scanln(&in_url)
 
-    urlcheck(in_url)
+	rurl, err := urlcheck(in_url)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println(rurl)
 }
 
-func urlcheck(in_url string) bool {
-    URL, err :=url.Parse(in_url)
-    if err != nil {
-        log.Panic(err)
-    }
-    fmt.Println(URL.Host)
-    return true
+func urlcheck(in_url string) (string, error) {
+	var short_url string
+	URL, err := url.Parse(in_url)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	if URL.Host != "www.amazon.in" {
+		return "", errors.New("Host of the url should be 'www.amazon.in'.")
+	}
+	if URL.Scheme != "https" {
+		return "", errors.New("Should be https.")
+	}
+	str_path := strings.Split(URL.Path, "/")
+	short_url = URL.Scheme + "://" + URL.Host + "/" + str_path[2] + "/" + str_path[3]
+	return short_url, nil
 }
