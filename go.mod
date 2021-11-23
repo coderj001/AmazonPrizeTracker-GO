8a230704f94a482c95caadf6023eceb5c1725945
coderj001 <amirajubolchi001@gmail.com>
2021-11-23 23:50:12 +0530

    added goquery mod and implement http

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/coderj001/AmazonPrizeTracker-GO.git
 
 go 1.16
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	golang.org/x/net v0.0.0-20211118161319-6a13c67c3ce4 // indirect
+)
--- main.go
+++ main.go
@@ -4,8 +4,12 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"net/http"
 	"net/url"
 	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
@@ -19,9 +23,16 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
-	fmt.Println(rurl)
+
+	response := scrapeweb(rurl)
+	fmt.Println(response.Status)
+	doc, _ := goquery.NewDocumentFromReader(response.Body)
+	title := doc.Find("span .a-size-large .product-title-word-break")
+	fmt.Println(title.Text())
 }
 
+// convert bulk url into sutiable formant
+// ex: http://www.amazon.com/dp/JA12AN120
 func urlcheck(in_url string) (string, error) {
 	var short_url string
 	URL, err := url.Parse(in_url)
@@ -38,4 +49,16 @@ func urlcheck(in_url string) (string, error) {
 	str_path := strings.Split(URL.Path, "/")
 	short_url = URL.Scheme + "://" + URL.Host + "/" + str_path[2] + "/" + str_path[3]
 	return short_url, nil
+}
+
+func scrapeweb(short_url string) *http.Response {
+	client := http.Client{Timeout: 30 * time.Second}
+
+	resp, err := client.Get(short_url)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer resp.Body.Close()
+
+	return resp
 }
